922f1fcad083ed9388c36f1d2297e5b21a838a24
Dallas Hart <36043275+Nomalah@users.noreply.github.com>
2023-07-31 10:29:01 -0400

    refactor: adapt book_metadata to new cancelable fetch

--- src/composables/book_metadata.ts
+++ src/composables/book_metadata.ts
@@ -1,12 +1,19 @@
-import { useCachedJSONFetch, type UseCachedFetchOptions } from "@/composables/cached_fetch";
+import { useJSONFetch } from "@/composables/cancelable_fetch";
 import { prepackaged_books, includes } from "@/scripts/constants";
 import type { BookSummary, BookIndex, SongList } from "@/scripts/types";
-import { computed, ref } from "vue";
 import type { BookReference } from "@/scripts/constants";
 import { Capacitor } from "@capacitor/core";
 
-export function getBookUrl(book_short_name: BookReference, fallback = false): string {
-    // If on the web, always use the local copy of the book
+function getRemoteBookUrl(book_short_name: BookReference): string {
+    return `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${import.meta.env.VITE_GIT_BRANCH}/` + "books/" + book_short_name;
+}
+
+function getLocalBookUrl(book_short_name: BookReference): string {
+    return import.meta.env.BASE_URL + "books/" + book_short_name;
+}
+
+export function getBookUrl(book_short_name: BookReference): string {
+    // If on the web, always use the local copy of the book (which also is the remote copy)
     if (Capacitor.getPlatform() == "web") {
         return getLocalBookUrl(book_short_name);
     }
@@ -21,60 +28,28 @@ export function getBookUrl(book_short_name: BookReference, fallback = false): st
         return getLocalBookUrl(book_short_name);
     }
 
-    // If falling back because of a failure use the local version of the book
-    if (fallback) {
-        return getLocalBookUrl(book_short_name);
-    }
-
     return getRemoteBookUrl(book_short_name);
 }
 
-function getRemoteBookUrl(book_short_name: BookReference): string {
-    return `https://raw.githubusercontent.com/ACC-Hymns/acchymns-web/${import.meta.env.VITE_GIT_BRANCH}/` + "books/" + book_short_name;
-}
-
-function getLocalBookUrl(book_short_name: BookReference): string {
-    return import.meta.env.BASE_URL + "books/" + book_short_name;
+export function getSongSrc(book: BookSummary, song_num: string): string {
+    const fileName = song_num + "." + book.fileExtension;
+    return `${getBookUrl(book.name.short as BookReference)}/songs/${fileName}`;
 }
 
 export function useBookSummary(book_short_name: BookReference) {
     const url = getBookUrl(book_short_name);
-    const summary = ref<BookSummary | null>(null);
-
-    async function execute() {
-        const result = await fetch(url + "/summary.json");
-        summary.value = await result.json();
-    }
-
-    execute();
-
-    return { summary };
+    const { data: summary, ...rest } = useJSONFetch<BookSummary>(url + "/summary.json", {});
+    return { summary, ...rest };
 }
 
 export function useBookSongList(book_short_name: BookReference) {
     const url = getBookUrl(book_short_name);
-    const song_list = ref<SongList | null>(null);
-
-    async function execute() {
-        const result = await fetch(url + "/songs.json");
-        song_list.value = await result.json();
-    }
-
-    execute();
-
-    return { song_list };
+    const { data: list, ...rest } = useJSONFetch<SongList>(url + "/songs.json", {});
+    return { list, ...rest };
 }
 
 export function useBookIndex(book_short_name: BookReference) {
     const url = getBookUrl(book_short_name);
-    const index = ref<BookIndex | null>(null);
-
-    async function execute() {
-        const result = await fetch(url + "/index.json");
-        index.value = await result.json();
-    }
-
-    execute();
-
-    return { index };
+    const { data: index, ...rest } = useJSONFetch<BookIndex>(url + "/index.json", {});
+    return { index, ...rest };
 }
